6aa2163806575e913048a39873ab260ef620d63e
Sri Manikandan R <srimanikandandev@gmail.com>
2024-07-29 09:39:15 +0530

    uploading the base project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Home/Header"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-black via-gray-950 to-gray-900">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='sticky top-0 shadow-md z-20'>
+        <div className="flex justify-between items-center bg-black text-white w-full h-[75px] p-8">
+            <Link to='/'><h1 className="text-2xl font-semibold">Mario</h1></Link>
+            <div className="flex justify-center items-center text-lg gap-5">
+                <Link to='/login'><a>Login</a></Link>
+                <Link to='/register'><a>Register</a></Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
