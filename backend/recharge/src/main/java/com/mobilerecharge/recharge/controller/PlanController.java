4d1202701402d495ac1b351364d946359d6aebf6
Sai Sidharthan <saisidharthan@gamil.com>
2024-08-05 11:50:27 +0530

    new field added

--- backend/recharge/src/main/java/com/mobilerecharge/recharge/controller/PlanController.java
+++ backend/recharge/src/main/java/com/mobilerecharge/recharge/controller/PlanController.java
@@ -15,34 +15,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
-
 @RestController
-@RequestMapping("/plans")
 public class PlanController {
     @Autowired
     private PlansServices service;
 
-    @GetMapping
+    @GetMapping("/plans")
     public List<PlansModel> getPlans()
     {
         return service.getallPlans();
     }
     
-    @PostMapping
+    @PostMapping("/plans")
     public ResponseEntity<PlansModel> addPlan(@RequestBody PlansModel plan)
     {
         PlansModel newPlan = service.addPlan(plan);
         return ResponseEntity.ok(newPlan);
     }
-    @PostMapping
+
+    @PostMapping("/addPostmanPlan")
     public ResponseEntity<List<PlansModel>> addPostmanPlan(@RequestBody List<PlansModel> plan)
     {
         List<PlansModel> newPlan = service.addPostmanPlan(plan);
         return ResponseEntity.ok(newPlan);
     }
 
-    @GetMapping("/planCount")
+    @GetMapping("/plans/planCount")
     public long getPlanCount(){
         return service.getPlanCount();
     }
